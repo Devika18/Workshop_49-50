c325eea1833a6cb058bf5a5a9796f4202946585e
Devika18 <devikankarpe@gmail.com>
2023-01-12 00:50:05 +0530

    added: logic for updating addressbook contact details

--- home.js
+++ home.js
@@ -3,6 +3,7 @@ window.addEventListener('DOMContentLoaded',(even) => {
     addressBookList = getAddressBookDataFromStorage();
     document.querySelector(".person-count").textContent = addressBookList.length;
     createInnerHtml();
+    localStorage.removeItem('editBook');
     });
     const getAddressBookDataFromStorage = () => {
         return localStorage.getItem('AddressBookList') ?
@@ -24,11 +25,32 @@ window.addEventListener('DOMContentLoaded',(even) => {
             <td>${addressBookData._zip}</td>
             <td>${addressBookData._phoneNumber}</td>
             <td>
-            <img name="${addressBookData._name}" onclick="remove(this)" alt="delete" src="delete-black-18dp.svg">
-            <img name="${addressBookData._name}" onclick="update(this)" alt="edit" src="create-black-18dp.svg">
+            <img id="${addressBookData._name}" onclick="remove(this)" alt="delete" src="./assests/icons/delete-black-18dp.svg">
+            <img id="${addressBookData._name}" onclick="update(this)" alt="edit" src="./assests/icons/create-black-18dp.svg">
             </td>
          </tr>
         `;
         }
         document.querySelector('#display').innerHTML=innerHtml;
     } 
+    const remove = (node) => {
+        let addressBookData = addressBookList.find(personData => personData._name == node.id);
+        if (!addressBookData) return;
+        const index = addressBookList
+                      .map(personData => personData._name)
+                      .indexOf(addressBookData._name);
+        addressBookList.splice(index, 1);
+        localStorage.setItem("AddressBookList", JSON.stringify(addressBookList));
+        document.querySelector(".person-count").textContent=addressBookList.length;
+        createInnerHtml();
+    } 
+    const update = (node) => {
+        let addressBookData = addressBookList.find(personData => personData._name == node.id);
+        if(!addressBookData) return;
+        localStorage.setItem('editBook', JSON.stringify(addressBookData));
+        window.location.replace(site_properties.add_person_page);
+    } 
+    let site_properties={
+        home_page: "home.html",
+        add_person_page: "AddPersonAddressBook.html"
+    };
